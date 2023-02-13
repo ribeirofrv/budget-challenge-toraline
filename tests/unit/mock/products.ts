df989b5cd49f3cbbe1bcf0d1d13a7815b72481c4
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-12 22:04:09 -0300

    test: camada de pcontroller de products

---
+++ tests/unit/mock/products.ts
@@ -0,0 +1,502 @@
+export default [
+  {
+    "id": 1,
+    "name": "explicabo alias hic reprehenderit deleniti quos id reprehenderit consequuntur ipsam iure voluptatem ea culpa excepturi ducimus repudiandae ab",
+    "price": 6945
+  },
+  {
+    "id": 2,
+    "name": "nostrum veritatis reprehenderit repellendus vel numquam soluta ex inventore ex",
+    "price": 2435
+  },
+  {
+    "id": 3,
+    "name": "praesentium explicabo reprehenderit laudantium a pariatur ab sit pariatur quos",
+    "price": 4985
+  },
+  {
+    "id": 4,
+    "name": "omnis cumque laudantium perspiciatis nostrum inventore mollitia natus quas vero reprehenderit reprehenderit ad",
+    "price": 3316
+  },
+  {
+    "id": 5,
+    "name": "eum repudiandae distinctio placeat autem perspiciatis architecto reprehenderit laborum assumenda consequatur est iusto",
+    "price": 4302
+  },
+  {
+    "id": 6,
+    "name": "accusantium a aperiam itaque",
+    "price": 11958
+  },
+  {
+    "id": 7,
+    "name": "doloremque beatae vitae nam est reiciendis a ut veritatis animi a laudantium ab reiciendis veniam dolores consectetur asperiores in",
+    "price": 5024
+  },
+  {
+    "id": 8,
+    "name": "consequuntur ratione a perferendis quaerat reprehenderit a dignissimos id",
+    "price": 10936
+  },
+  {
+    "id": 9,
+    "name": "illo quo",
+    "price": 5637
+  },
+  {
+    "id": 10,
+    "name": "delectus at consequuntur a a quod est a",
+    "price": 7202
+  },
+  {
+    "id": 11,
+    "name": "aliquid reprehenderit consequuntur corporis mollitia nostrum et reprehenderit ea eveniet consequatur doloremque a id",
+    "price": 3389
+  },
+  {
+    "id": 12,
+    "name": "eveniet voluptatibus reprehenderit ad doloremque impedit doloremque maxime assumenda quae a a dolor illum",
+    "price": 7609
+  },
+  {
+    "id": 13,
+    "name": "voluptatibus a a modi similique ea in laudantium praesentium",
+    "price": 4388
+  },
+  {
+    "id": 14,
+    "name": "eligendi consequatur atque voluptatibus",
+    "price": 13620
+  },
+  {
+    "id": 15,
+    "name": "quia perspiciatis provident reprehenderit molestiae ab architecto a aspernatur dolorem a et placeat molestias a",
+    "price": 11776
+  },
+  {
+    "id": 16,
+    "name": "dolores similique perspiciatis excepturi doloribus reprehenderit id molestias nihil ipsa",
+    "price": 903
+  },
+  {
+    "id": 17,
+    "name": "delectus optio reprehenderit illo velit a excepturi",
+    "price": 14350
+  },
+  {
+    "id": 18,
+    "name": "vel minus voluptatibus voluptatibus accusantium qui",
+    "price": 14328
+  },
+  {
+    "id": 19,
+    "name": "nam inventore omnis sapiente at quae soluta voluptatum reprehenderit ipsam blanditiis iusto sint quod consequatur qui",
+    "price": 7113
+  },
+  {
+    "id": 20,
+    "name": "possimus reprehenderit voluptatum ex praesentium blanditiis quod reprehenderit",
+    "price": 9847
+  },
+  {
+    "id": 21,
+    "name": "ratione ea molestias reprehenderit placeat nam recusandae reprehenderit quia a blanditiis commodi",
+    "price": 11760
+  },
+  {
+    "id": 22,
+    "name": "alias vero optio voluptatibus ad doloremque asperiores a corrupti nemo",
+    "price": 10021
+  },
+  {
+    "id": 23,
+    "name": "reprehenderit a vero in reprehenderit unde unde asperiores at nesciunt voluptatibus qui consequuntur a sint perspiciatis",
+    "price": 8299
+  },
+  {
+    "id": 24,
+    "name": "minima a optio asperiores vel pariatur vel reprehenderit a voluptatem vel quo et magnam eveniet hic voluptate reiciendis sit a",
+    "price": 206
+  },
+  {
+    "id": 25,
+    "name": "dolores est reprehenderit incidunt libero in facilis a a reprehenderit reprehenderit sint nam provident",
+    "price": 7846
+  },
+  {
+    "id": 26,
+    "name": "a accusantium vel assumenda",
+    "price": 14846
+  },
+  {
+    "id": 27,
+    "name": "quo a quod ad atque a ab reprehenderit sequi libero consequatur consequuntur reprehenderit at",
+    "price": 14281
+  },
+  {
+    "id": 28,
+    "name": "laborum reprehenderit perspiciatis repudiandae iure veniam a aut assumenda a",
+    "price": 4693
+  },
+  {
+    "id": 29,
+    "name": "repellat illo tenetur voluptatibus velit reiciendis a tempore repudiandae tenetur explicabo tempore reprehenderit cupiditate ab cum",
+    "price": 14907
+  },
+  {
+    "id": 30,
+    "name": "consequuntur mollitia veritatis id voluptatibus atque aperiam laborum ex voluptatibus quasi perspiciatis ut",
+    "price": 11868
+  },
+  {
+    "id": 31,
+    "name": "voluptatibus itaque voluptatibus reprehenderit aspernatur veniam quae",
+    "price": 12414
+  },
+  {
+    "id": 32,
+    "name": "a vitae autem autem a",
+    "price": 6125
+  },
+  {
+    "id": 33,
+    "name": "consequuntur tempore quo",
+    "price": 10046
+  },
+  {
+    "id": 34,
+    "name": "mollitia incidunt asperiores amet nostrum assumenda nam fuga",
+    "price": 10566
+  },
+  {
+    "id": 35,
+    "name": "esse porro a praesentium hic",
+    "price": 7124
+  },
+  {
+    "id": 36,
+    "name": "quis repellendus fugiat voluptatem",
+    "price": 12334
+  },
+  {
+    "id": 37,
+    "name": "perspiciatis ex",
+    "price": 12189
+  },
+  {
+    "id": 38,
+    "name": "qui impedit facilis id",
+    "price": 4596
+  },
+  {
+    "id": 39,
+    "name": "reprehenderit reprehenderit repudiandae soluta quibusdam reprehenderit ut ex repellat a hic",
+    "price": 750
+  },
+  {
+    "id": 40,
+    "name": "reprehenderit asperiores reprehenderit dolore doloribus perspiciatis veniam repudiandae dolorem a",
+    "price": 11545
+  },
+  {
+    "id": 41,
+    "name": "porro",
+    "price": 11747
+  },
+  {
+    "id": 42,
+    "name": "ab quo ea incidunt eum voluptatem provident fuga assumenda omnis consequatur eius reprehenderit vel illum veritatis laboriosam saepe aut libero animi",
+    "price": 13445
+  },
+  {
+    "id": 43,
+    "name": "reprehenderit voluptatum reprehenderit reprehenderit accusamus laboriosam inventore debitis ea minima eveniet ut nemo",
+    "price": 4286
+  },
+  {
+    "id": 44,
+    "name": "natus consequatur reprehenderit quo",
+    "price": 1542
+  },
+  {
+    "id": 45,
+    "name": "asperiores vitae voluptate perspiciatis unde temporibus voluptatibus placeat a corporis natus praesentium sint",
+    "price": 4799
+  },
+  {
+    "id": 46,
+    "name": "consectetur nemo magnam libero quibusdam ipsa temporibus provident in vel aliquam accusamus a",
+    "price": 7726
+  },
+  {
+    "id": 47,
+    "name": "delectus reprehenderit nam quam molestias a magnam voluptate",
+    "price": 6589
+  },
+  {
+    "id": 48,
+    "name": "quisquam excepturi odio consequatur corrupti a ad hic illo a in fugit voluptatibus rem corporis optio a atque",
+    "price": 1736
+  },
+  {
+    "id": 49,
+    "name": "voluptatem autem repellendus facere consectetur accusantium reprehenderit harum corporis mollitia veniam ut",
+    "price": 8701
+  },
+  {
+    "id": 50,
+    "name": "facilis obcaecati a",
+    "price": 12477
+  },
+  {
+    "id": 51,
+    "name": "reprehenderit perspiciatis ad",
+    "price": 10808
+  },
+  {
+    "id": 52,
+    "name": "a consectetur explicabo voluptatum repellat corporis esse perspiciatis id",
+    "price": 4047
+  },
+  {
+    "id": 53,
+    "name": "voluptatibus accusantium ducimus officiis nostrum",
+    "price": 14179
+  },
+  {
+    "id": 54,
+    "name": "temporibus a placeat odit facilis quidem quam",
+    "price": 8741
+  },
+  {
+    "id": 55,
+    "name": "reiciendis qui a labore perferendis odio a illo doloremque expedita placeat asperiores est",
+    "price": 6062
+  },
+  {
+    "id": 56,
+    "name": "veniam sunt consectetur est veniam aut et ut tenetur doloribus magnam",
+    "price": 6343
+  },
+  {
+    "id": 57,
+    "name": "beatae reprehenderit velit a repudiandae ab reprehenderit est a soluta architecto ut cupiditate dignissimos assumenda",
+    "price": 4306
+  },
+  {
+    "id": 58,
+    "name": "omnis a",
+    "price": 11295
+  },
+  {
+    "id": 59,
+    "name": "odit explicabo consectetur neque veritatis a",
+    "price": 6666
+  },
+  {
+    "id": 60,
+    "name": "fugit voluptatibus dolorum delectus at",
+    "price": 6613
+  },
+  {
+    "id": 61,
+    "name": "in id voluptatibus at vel ea consectetur quidem est tempore sed dolorem sit ea deleniti beatae",
+    "price": 14136
+  },
+  {
+    "id": 62,
+    "name": "vero cum provident natus a reprehenderit deserunt consequatur sed dignissimos quisquam",
+    "price": 9226
+  },
+  {
+    "id": 63,
+    "name": "ipsam itaque sed quas a ipsum possimus id harum a a consequuntur ad maiores",
+    "price": 13816
+  },
+  {
+    "id": 64,
+    "name": "ea quis",
+    "price": 8558
+  },
+  {
+    "id": 65,
+    "name": "perspiciatis quibusdam",
+    "price": 5083
+  },
+  {
+    "id": 66,
+    "name": "inventore consectetur consequuntur officia esse",
+    "price": 13410
+  },
+  {
+    "id": 67,
+    "name": "magnam",
+    "price": 14580
+  },
+  {
+    "id": 68,
+    "name": "reprehenderit reprehenderit impedit ducimus velit perspiciatis aperiam voluptatibus sit illum",
+    "price": 8729
+  },
+  {
+    "id": 69,
+    "name": "reprehenderit a",
+    "price": 10884
+  },
+  {
+    "id": 70,
+    "name": "dolorum reprehenderit veritatis",
+    "price": 6636
+  },
+  {
+    "id": 71,
+    "name": "repudiandae officiis voluptatem facilis expedita aliquid ab a minus numquam",
+    "price": 12852
+  },
+  {
+    "id": 72,
+    "name": "et voluptatibus delectus",
+    "price": 11337
+  },
+  {
+    "id": 73,
+    "name": "perferendis reprehenderit ab nobis veritatis nam ex ipsa perspiciatis ipsa mollitia optio ducimus repudiandae officia",
+    "price": 9488
+  },
+  {
+    "id": 74,
+    "name": "aut reprehenderit dignissimos perspiciatis a facere cum quisquam optio ducimus assumenda architecto",
+    "price": 5753
+  },
+  {
+    "id": 75,
+    "name": "placeat obcaecati a alias a perspiciatis aut repellendus a voluptate repudiandae architecto",
+    "price": 2560
+  },
+  {
+    "id": 76,
+    "name": "soluta in quo",
+    "price": 12603
+  },
+  {
+    "id": 77,
+    "name": "eligendi quidem doloribus a ex autem reprehenderit ipsam aliquid consequuntur architecto reprehenderit delectus ut",
+    "price": 3384
+  },
+  {
+    "id": 78,
+    "name": "incidunt a aliquid veniam consequuntur eos minima",
+    "price": 14568
+  },
+  {
+    "id": 79,
+    "name": "accusantium iusto tenetur dolorum perspiciatis recusandae perspiciatis perspiciatis praesentium libero facilis veritatis inventore dolore sit a",
+    "price": 1996
+  },
+  {
+    "id": 80,
+    "name": "iusto officiis ab",
+    "price": 2086
+  },
+  {
+    "id": 81,
+    "name": "a in reprehenderit reiciendis nam deleniti accusamus asperiores nulla consequuntur hic",
+    "price": 193
+  },
+  {
+    "id": 82,
+    "name": "explicabo ut",
+    "price": 11714
+  },
+  {
+    "id": 83,
+    "name": "perspiciatis provident repellendus recusandae veritatis fugit repudiandae a",
+    "price": 6800
+  },
+  {
+    "id": 84,
+    "name": "dignissimos laboriosam cumque laboriosam reprehenderit perspiciatis cum delectus perspiciatis eum eveniet enim a",
+    "price": 14188
+  },
+  {
+    "id": 85,
+    "name": "consectetur quae praesentium tempora quae illo dolorem a modi ducimus ipsa a hic a",
+    "price": 13015
+  },
+  {
+    "id": 86,
+    "name": "a recusandae cum aspernatur dolores asperiores reprehenderit et ad nostrum accusantium a reprehenderit nihil a voluptatibus sed delectus commodi",
+    "price": 6361
+  },
+  {
+    "id": 87,
+    "name": "doloremque ipsum voluptatem velit",
+    "price": 13287
+  },
+  {
+    "id": 88,
+    "name": "reprehenderit consequuntur vero",
+    "price": 13509
+  },
+  {
+    "id": 89,
+    "name": "vitae unde adipisci excepturi minus perferendis incidunt a voluptatibus illo",
+    "price": 3330
+  },
+  {
+    "id": 90,
+    "name": "dignissimos repudiandae a repudiandae earum excepturi reprehenderit dolorum",
+    "price": 10461
+  },
+  {
+    "id": 91,
+    "name": "aliquam reprehenderit reprehenderit facilis sequi cum excepturi reprehenderit reprehenderit a a",
+    "price": 10269
+  },
+  {
+    "id": 92,
+    "name": "cupiditate inventore provident expedita",
+    "price": 9922
+  },
+  {
+    "id": 93,
+    "name": "ipsam sequi praesentium magnam libero at in asperiores perspiciatis odio quisquam reprehenderit perspiciatis magni modi repudiandae",
+    "price": 11003
+  },
+  {
+    "id": 94,
+    "name": "reprehenderit provident id",
+    "price": 3130
+  },
+  {
+    "id": 95,
+    "name": "dolorem perferendis labore id iusto vero a quasi doloribus ea illum earum reprehenderit voluptatem a repellat explicabo",
+    "price": 9984
+  },
+  {
+    "id": 96,
+    "name": "possimus pariatur animi alias velit",
+    "price": 837
+  },
+  {
+    "id": 97,
+    "name": "aliquam dolorum distinctio ad perferendis reprehenderit consectetur ea dolore a in cum sequi et voluptatem quasi",
+    "price": 1992
+  },
+  {
+    "id": 98,
+    "name": "ducimus ad architecto cupiditate aliquid magnam",
+    "price": 12442
+  },
+  {
+    "id": 99,
+    "name": "laudantium voluptate reprehenderit a facere hic reprehenderit perspiciatis cum odit ratione corrupti",
+    "price": 5293
+  },
+  {
+    "id": 100,
+    "name": "eos sapiente officia dolores deserunt",
+    "price": 12999
+  }
+]
