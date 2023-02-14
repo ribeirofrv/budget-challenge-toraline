54ad5a6e7867af7e903618b7bd0a3a472a447b86
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-14 00:09:53 -0300

    feat: cria rota para usuario obter o valor total de compras

--- src/Router/User.ts
+++ src/Router/User.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import UsersController from '../Controller/UserController';
+import BudgetController from '../Controller/BudgetController';
 
 const usersRouter = Router();
 
 usersRouter.get('/', (req, res, next) =>
   new UsersController(req, res, next).getUsers());
+
+usersRouter.post('/:userId/budget', (req, res, next) =>
+  new BudgetController(req, res, next).getBudget());
 
 export default usersRouter;
