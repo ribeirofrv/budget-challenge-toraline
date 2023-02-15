fbeaa70db0303c0794fc693e5135f97098bea5f3
Francisca Ribeiro <ribeirofrv@gmail.com>
2023-02-14 22:55:04 -0300

    feat: cria metodo para calcular taxa/preço por produto

--- src/Interface/IOrder.ts
+++ src/Interface/IOrder.ts
@@ -1,6 +1,11 @@
-import { IProductUnit } from './IProduct';
+import { IProductUnit, IProduct } from './IProduct';
 
 export default interface IOrder {
   userId: number;
   products: IProductUnit[];
+}
+
+export interface IOrderBudget {
+  userId: number;
+  products: IProduct[];
 }
--- src/Service/Budget.ts
+++ src/Service/Budget.ts
@@ -1,20 +1,23 @@
-import IOrder from '../Interface/IOrder';
+import { IOrderBudget } from '../Interface/IOrder';
 import IUser from '../Interface/IUser';
-import { IProduct, IProductUnit } from '../Interface/IProduct';
+import { IProduct } from '../Interface/IProduct';
 import ExternalApi from './ExternalApi';
 import HttpException from '../Error/HttpException';
 
 export default class Budget extends ExternalApi {
-  async getBudget(order: IOrder) {
+  async getBudget(order: IOrderBudget) {
     const { userId, products } = order;
     const tax = this.getTaxByUserId(userId);
-    const productPrice = this.getProductPrice(products);
-    const budget = await Promise.all([tax, productPrice]);
-    if (!budget) {
+    const feePerProduct = this.getFeePerProduct(order);
+    const totalPrice = this.getProductPrice(products);
+    const budget = await Promise.all([tax, totalPrice, feePerProduct]);
+    
+    if (!products || !products.length) {
       throw new HttpException(400, 'Unable to get quote, something went');
     }
     const total = {
-      budget: (budget[1] * budget[0]).toFixed(2),
+      ...budget[2], // userId, products
+      finalPrice: parseFloat((budget[1] * (1 + budget[0] / 100)).toFixed(2)), // total price
     };
     return total;
   }
@@ -27,16 +30,32 @@ export default class Budget extends ExternalApi {
     return userTaxDecimal;
   }
 
-  async getProductPrice(products: IProductUnit[]) {
+  async getProductPrice(products: IProduct[]) {
     const getProducts = await this.getProducts();
     const productPrice = products.reduce((acc, curr) => {
       const currProduct = getProducts.find(
         (product: IProduct) => product.id === curr.id,
       );
-      return acc + (currProduct.price * curr.quantity);
+      return acc + currProduct.price;
     }, 0);
-    console.log('productPrice', productPrice);
-
     return productPrice;
+  }
+
+  async getFeePerProduct(order: IOrderBudget) {
+    const tax = await this.getTaxByUserId(order.userId);
+    const allProducts = await this.getProducts();
+    const products = order.products.map((product) => {
+      const findProduct = allProducts.find(
+        (p: IProduct) => p.id === product.id,
+      );
+      const total = (findProduct.price * (1 + tax)).toFixed(2);
+      return {
+        name: findProduct.name,
+        price: findProduct.price,
+        tax: `${tax * 100}%`,
+        total: parseFloat(total),
+      };
+    });
+    return { userId: order.userId, products };
   }
 }
